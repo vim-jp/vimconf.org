0dda575f76df371dbf141397fcc864c3013b1e01
Shintaro Abe <dictav@gmail.com>
2013-11-01 11:04:10 +0900

    Add the ability to update the 'updated_at' attribute

--- 2013/data/compile_schedule.rb
+++ 2013/data/compile_schedule.rb
@@ -2,6 +2,7 @@ require 'yaml'
 require 'json'
 abort 'no support for legacy ruby' unless __dir__
 y = YAML.load_file "#{__dir__}/schedules.yaml"
+y["updated_at"] = Time.new.to_i
 File.open("#{__dir__}/schedules.json", 'w') do |io|
   io.write y.to_json
 end
