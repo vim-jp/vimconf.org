0d58056bf89710272b050cbb228240c6cc3b179e
Shintaro Abe <dictav@gmail.com>
2013-11-10 13:04:14 +0900

    Upgrade compile_schedule.rb to set scheduled_at parameter automatically

--- 2013/data/compile_schedule.rb
+++ 2013/data/compile_schedule.rb
@@ -3,6 +3,18 @@ require 'json'
 abort 'no support for legacy ruby' unless __dir__
 y = YAML.load_file "#{__dir__}/schedules.yaml"
 y["updated_at"] = Time.new.to_i
+scheduled_at = 0
+y["schedules"].each do |a|
+	if scheduled_at == 0
+		scheduled_at = a["scheduled_at"]
+	else
+		a["scheduled_at"] = scheduled_at
+	end
+
+	a["time"].match /(\d+) minutes/
+	min = $1.to_i
+	scheduled_at += min*60
+end
 File.open("#{__dir__}/schedules.json", 'w') do |io|
-  io.write y.to_json
+	io.write y.to_json
 end
