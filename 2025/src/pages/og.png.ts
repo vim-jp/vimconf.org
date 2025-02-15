b8c11e53013b99d130d705e44f1c22a21cf06fae
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-02-15 20:16:22 +0000

    feat: generate og with satori/sharp

---
+++ 2025/src/pages/og.png.ts
@@ -0,0 +1,11 @@
+import { ogImage } from '@/components/OgImage';
+
+export async function GET() {
+  const body = await ogImage();
+
+  return new Response(body, {
+    headers: {
+      'content-type': 'image/png',
+    },
+  });
+}
