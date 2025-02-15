37c5b789a7beb37f088f7b3a6871dfddb7c29987
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-02-15 19:15:12 +0000

    fix: use background color as constats

--- 2025/src/consts/index.ts
+++ 2025/src/consts/index.ts
@@ -3,5 +3,6 @@ import { base } from "astro:config/client"
 
 import { DOMAIN } from "./_domain.js" with { type: "macro" }
 
-
 export const BASE_URL = ufo.joinURL(DOMAIN, base)
+
+export const MAIN_BACKGROUND_COLOR = `#069700`;
