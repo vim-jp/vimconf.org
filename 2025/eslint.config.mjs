1b54fbb79b912ce335a82c0c58c5cc392bc86023
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-02-16 10:50:14 +0000

    chore(2025): format

--- 2025/eslint.config.mjs
+++ 2025/eslint.config.mjs
@@ -1,10 +1,10 @@
-import { ryoppippi } from "@ryoppippi/eslint-config";
+import { ryoppippi } from '@ryoppippi/eslint-config';
 
 export default ryoppippi({
-  type: 'app', // or 'lib'
-  svelte: false,
-  astro: true,
-  typescript: {
-    tsconfigPath: "./tsconfig.json",
-  },
+	type: 'app', // or 'lib'
+	svelte: false,
+	astro: true,
+	typescript: {
+		tsconfigPath: './tsconfig.json',
+	},
 });
