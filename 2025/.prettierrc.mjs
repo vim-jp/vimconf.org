f14cd9227ede4610ab706ba1697ea81f2afe8985
staticWagomU <to6wa17@gmail.com>
2025-05-19 22:29:26 +0900

    chore: prettierの設定を追加

--- 2025/.prettierrc.mjs
+++ 2025/.prettierrc.mjs
@@ -1,22 +1,21 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
-  tailwindStylesheet: "src/styles/global.css",
-  tailwindConfig: "tailwind.config.ts",
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-    {
-      files: "*.svelte",
-      options: {
-        parser: "svelte",
-      },
-    },
-  ],
+	plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+	tailwindStylesheet: 'src/styles/global.css',
+	tailwindConfig: 'tailwind.config.ts',
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+		{
+			files: '*.svelte',
+			options: {
+				parser: 'svelte',
+			},
+		},
+	],
 };
-
