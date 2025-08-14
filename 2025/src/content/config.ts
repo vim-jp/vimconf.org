77cb8f22b34f7b1587e7e3bcacb780971353ce19
staticWagomU <to6wa17@gmail.com>
2025-08-14 14:37:04 +0900

    feat: timetableのスキーマを追加

--- 2025/src/content/config.ts
+++ 2025/src/content/config.ts
@@ -1,5 +1,4 @@
-import { glob } from "astro/loaders";
-// src/content/config.ts
+import { file, glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
 const sponsors = defineCollection({
@@ -15,7 +14,20 @@ const sponsors = defineCollection({
   }),
 });
 
-// コレクションのエクスポート
+const timetable = defineCollection({
+  loader: file("./src/content/timetable.toml"),
+  schema: z.array(
+    z.object({
+      title: z.string(),
+      start_at: z.coerce.date(),
+      end_at: z.coerce.date(),
+      speaker_name: z.string().optional(),
+      session_id: z.string().optional(),
+    }),
+  ),
+});
+
 export const collections = {
   sponsors,
+  timetable,
 };
