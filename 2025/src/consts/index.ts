f58e7a3a0d2dd69bbccf236c6cd07cc9384d4d41
MURAOKA Taro <koron.kaoriya@gmail.com>
2026-05-19 11:20:07 +0900

    Replace the content of 2025/ with statically generated content

--- 2025/src/consts/index.ts
+++
@@ -1,8 +0,0 @@
-import { base } from "astro:config/client";
-import * as ufo from "ufo";
-
-import { DOMAIN } from "./_domain.js" with { type: "macro" };
-
-export const BASE_URL = ufo.joinURL(DOMAIN, base);
-
-export const MAIN_BACKGROUND_COLOR = `#069700`;
--- 2025/src/i18n/index.ts
+++
@@ -1,9 +0,0 @@
-export const LANGUAGES = ["ja", "en"] as const;
-
-type Lang = (typeof LANGUAGES)[number];
-
-export function useTranslate(
-  lang: Lang,
-): (...translations: string[]) => string {
-  return (...translations: string[]) => translations[LANGUAGES.indexOf(lang)];
-}
